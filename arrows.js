f20b729ba1e390d36304a8202cb285d73eed8d49
Paul Thomas <paulthomas2107@gmail.com>
2023-11-17 09:20:55 +0000

    sss

---
+++ arrows.js
@@ -0,0 +1,24 @@
+const hello = function () {
+  console.log('Hello');
+};
+
+// Better
+const hello2 = (name, age) => {
+  console.log(`Hello ${name}. You are ${age} years old.`);
+};
+
+hello();
+hello2('Paul', 23);
+
+setTimeout(
+  (name2, deposit) => {
+    console.log(`Hi, ${name2}. Deposit is ${deposit}`);
+  },
+  250,
+  'John',
+  123.45
+);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const squares = numbers.map((element) => Math.pow(element, 2));
+console.log(squares);
