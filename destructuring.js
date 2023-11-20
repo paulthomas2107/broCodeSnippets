de6dfb5e26b35b0f6b1a6001e39c0bd8157cd680
Paul Thomas <paulthomas2107@gmail.com>
2023-11-20 13:56:06 +0000

    ssss

---
+++ destructuring.js
@@ -0,0 +1,35 @@
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+console.log(a, b);
+
+const colors = ['red', 'green', 'blue', 'black', 'white'];
+console.log(colors);
+[colors[0], colors[4]] = [colors[4], colors[0]];
+console.log(colors);
+
+const [firstColor, secondColor, thirdColor, ...extraColors] = colors;
+console.log(firstColor, secondColor, thirdColor, extraColors);
+
+const person1 = {
+  firstName: 'SpongeBob',
+  lastName: 'SquarePants',
+  age: 30,
+  job: 'Fry Cook',
+};
+
+const person2 = {
+  firstName: 'Patrick',
+  lastName: 'Star',
+  age: 33,
+};
+
+const { firstName, lastName, age, job = 'Vacant' } = person2;
+console.log(firstName, lastName, age, job);
+
+const displayPerson = ({ firstName, lastName, age, job = 'Vacant' }) => {
+  console.log(firstName, lastName, age, job);
+};
+
+displayPerson(person1);
+displayPerson(person2);
