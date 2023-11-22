0cef7406d197c4b93459b6a7cec610ab7491fd89
Paul Thomas <paulthomas2107@gmail.com>
2023-11-22 16:48:20 +0000

    xxx

---
+++ shuffle.js
@@ -0,0 +1,26 @@
+const cards = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+];
+
+// Fisher-Yates Algorithm
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+};
+
+shuffle(cards);
+console.log(cards);
