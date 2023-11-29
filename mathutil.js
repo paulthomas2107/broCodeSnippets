36bc449af41c9c3b42658a4567fa8de86c55d897
Paul Thomas <paulthomas2107@gmail.com>
2023-11-29 17:48:34 +0000

    xxxx

---
+++ mathutil.js
@@ -0,0 +1,13 @@
+export const PI = 3.14159;
+
+export const getCircumference = (radius) => {
+  return 2 * PI * radius;
+};
+
+export const getArea = (radius) => {
+  return PI * radius * radius;
+};
+
+export const getVolume = (radius) => {
+  return 4 * PI * radius * radius;
+};
